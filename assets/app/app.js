b3b08f853a69e8b5e33141c2004d8d4e0224a31f
Noah Phillips <noahphilli@gmail.com>
2015-07-09 18:38:32 -0700

    [CONFIG] started work on responsiv layout and functionaly of main nav and sidebar

--- assets/app/app.js
+++ assets/app/app.js
@@ -1,6 +1,14 @@
 $(document).ready(function () {
+
   $('.subnav-trigger').on('click', function () {
     $('.sub-nav').removeClass('is-active')
     $('.sub-nav', this).toggleClass('is-active')
-  })
+  });
+
+  $('#menu-toggle').on('click', function () {
+    $('#menu-button').toggleClass('on')
+    // $(this).toggleClass('on')
+    $('.sidebar').toggleClass('show');
+  });
+
 });
